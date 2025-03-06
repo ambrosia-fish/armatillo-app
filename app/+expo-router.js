4e7ddf4536a8020c02c7067b6934a203d5791530
Josef Ameur <148804322+ambrosia-fish@users.noreply.github.com>
2025-03-06 13:10:36 -0500

    Add expo-router config for deep links

---
+++ app/+expo-router.js
@@ -0,0 +1,8 @@
+// app/+expo-router.js
+// This file configures expo-router
+// See https://docs.expo.dev/router/reference/deep-linking/
+
+// This adds support for deep links with the custom URI scheme "armatillo://"
+export default {
+  scheme: "armatillo",
+};
