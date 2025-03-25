c3f27998956e79d91335809354f77a5c501d56f0
Josef Ameur <148804322+ambrosia-fish@users.noreply.github.com>
2025-03-25 15:37:47 -0400

    Add components index file

---
+++ app/components/index.ts
@@ -0,0 +1,25 @@
+/**
+ * Components barrel file
+ * 
+ * Exports all themed and shared components for easy importing.
+ * Use this to import multiple components in a single line:
+ * 
+ * import { Button, Input, Card, Header } from './components';
+ */
+
+// Themed UI components
+export { default as Button } from './Button';
+export { default as Input } from './Input';
+export { default as Card } from './Card';
+export { default as Header } from './Header';
+
+// Authentication components
+export { default as AuthGuard } from './AuthGuard';
+
+// Existing components
+export { default as CancelFooter } from './CancelFooter';
+export { default as EmojiSelectionGrid } from './EmojiSelectionGrid';
+export { default as InstanceDetailsModal } from './InstanceDetailsModal';
+
+// Hook for using color scheme
+export { useColorScheme } from './useColorScheme';
