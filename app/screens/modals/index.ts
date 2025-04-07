8115f08519d4bf954cc310667d809b1b90d3514a
Josef Ameur <148804322+ambrosia-fish@users.noreply.github.com>
2025-04-07 09:43:59 -0400

    Add modals index.ts for component exports

---
+++ app/screens/modals/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Modals barrel file
+ * 
+ * Exports all modal components for easy importing.
+ * Use this to import multiple components in a single line:
+ * 
+ * import { TimePickerModal, DurationPickerModal } from '@/app/screens/modals';
+ */
+
+export { default as Modal } from './modal';
+export { default as DetailScreen } from './detail-screen';
+export { default as TimePickerModal } from './time-picker-modal';
+export { default as DurationPickerModal } from './duration-picker-modal';
