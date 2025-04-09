c4615c98f065803e500e0eaa3d7966a63a894d4b
Josef Ameur <148804322+ambrosia-fish@users.noreply.github.com>
2025-04-09 16:25:35 -0400

    Add nested index file to enforce home tab navigation

---
+++ app/(tabs)/index/index.tsx
@@ -0,0 +1,7 @@
+// This file ensures the Home tab acts as the true index of the app
+// It forwards to the main Home screen component defined in app/(tabs)/index.tsx
+
+import Home from '../index';
+
+// Re-export the Home component as the default
+export default Home;
