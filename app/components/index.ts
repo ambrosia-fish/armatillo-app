8a35e320f3f3d6dc86387ca488a1f67312dab553
Josef Ameur <148804322+ambrosia-fish@users.noreply.github.com>
2025-04-11 14:18:17 -0400

    Remove SimpleHeader export from components barrel file

--- app/components/index.ts
+++ app/components/index.ts
@@ -12,7 +12,6 @@ export { default as Button } from './Button';
 export { default as Input } from './Input';
 export { default as Card } from './Card';
 export { default as Header } from './Header';
-export { default as SimpleHeader } from './SimpleHeader';
 export { Text, View } from './Themed';
 
 // Authentication components
