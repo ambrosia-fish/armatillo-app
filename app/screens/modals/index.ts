63807b51378a306953ea9a32476d9aff6b07bf93
Josef Ameur <148804322+ambrosia-fish@users.noreply.github.com>
2025-04-17 11:20:20 -0400

    feat: add StrategyModal export to modals index

--- app/screens/modals/index.ts
+++ app/screens/modals/index.ts
@@ -11,3 +11,4 @@ export { default as TimePickerModal } from './time-picker-modal';
 export { default as DurationPickerModal } from './duration-picker-modal';
 export { default as InstanceDetailsModal } from './instance-details-modal';
 export { default as AnswerSelectorModal } from './answer-selector-modal';
+export { default as StrategyModal } from './strategy-modal';
