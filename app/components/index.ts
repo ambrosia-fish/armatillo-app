64ff858fa903b799b303aa9dca6a701a19d308d2
Josef Ameur <148804322+ambrosia-fish@users.noreply.github.com>
2025-05-05 14:06:29 -0400

    fix(components): add default export to components/index.ts

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,3 +1,16 @@
 export * from './common';
 export * from './forms';
 export * from './layout';
+
+// Adding a default export that includes all components
+import * as CommonComponents from './common';
+import * as FormComponents from './forms';
+import * as LayoutComponents from './layout';
+
+const Components = {
+  ...CommonComponents,
+  ...FormComponents,
+  ...LayoutComponents
+};
+
+export default Components;
