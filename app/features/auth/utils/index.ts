7f133602605fff253ab1f29f3950a368c3f1e742
Josef Ameur <148804322+ambrosia-fish@users.noreply.github.com>
2025-05-05 14:17:22 -0400

    fix(auth): add default export to auth/utils/index.ts

--- app/features/auth/utils/index.ts
+++ app/features/auth/utils/index.ts
@@ -1,2 +1,13 @@
 export * from './tokenRefresher';
 export * from './tokenUtils';
+
+import * as TokenRefresher from './tokenRefresher';
+import * as TokenUtils from './tokenUtils';
+
+// Export auth utilities as default
+const AuthUtils = {
+  ...TokenRefresher,
+  ...TokenUtils
+};
+
+export default AuthUtils;
