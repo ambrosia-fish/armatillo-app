9a017331a14fd3002161d47840144843eec6f44f
Josef Ameur <148804322+ambrosia-fish@users.noreply.github.com>
2025-05-05 14:16:11 -0400

    fix(constants): add default export to constants/index.ts

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,1 +1,9 @@
 export { default as OptionDictionaries } from './options';
+import OptionDictionaries from './options';
+
+// Export constants as default
+const Constants = {
+  OptionDictionaries
+};
+
+export default Constants;
