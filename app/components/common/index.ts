d40626e8097ff36cb9268cd6f326507524e3c26f
Josef Ameur <148804322+ambrosia-fish@users.noreply.github.com>
2025-05-05 14:06:35 -0400

    fix(components): add default export to common/index.ts

--- app/components/common/index.ts
+++ app/components/common/index.ts
@@ -4,3 +4,22 @@ export { default as Card } from './Card';
 export { default as Input } from './Input';
 export * from './Themed';
 export * from './Modals';
+
+// Create a default export of common components
+import AuthGuard from './AuthGuard';
+import Button from './Button';
+import Card from './Card';
+import Input from './Input';
+import * as Themed from './Themed';
+import * as Modals from './Modals';
+
+const Common = {
+  AuthGuard,
+  Button,
+  Card,
+  Input,
+  ...Themed,
+  ...Modals
+};
+
+export default Common;
