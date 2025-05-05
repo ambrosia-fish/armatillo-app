49475b1158f85d4489abf25f4fb158fda3fd49ae
Josef Ameur <148804322+ambrosia-fish@users.noreply.github.com>
2025-05-05 14:17:13 -0400

    fix(auth): add default export to auth/screens/index.ts

--- app/features/auth/screens/index.ts
+++ app/features/auth/screens/index.ts
@@ -1,1 +1,9 @@
 export { default as LoginScreen } from './LoginScreen/LoginScreen';
+import LoginScreen from './LoginScreen/LoginScreen';
+
+// Export auth screens as default
+const AuthScreens = {
+  LoginScreen
+};
+
+export default AuthScreens;
