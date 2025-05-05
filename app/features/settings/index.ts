655893c6e3ccb9291971a2e2ce64010e6f05ae09
Josef Ameur <148804322+ambrosia-fish@users.noreply.github.com>
2025-05-05 14:17:35 -0400

    fix(settings): add default export to settings/index.ts

--- app/features/settings/index.ts
+++ app/features/settings/index.ts
@@ -1,1 +1,9 @@
 export { default as SettingsScreen } from './screens/SettingsScreen/SettingsScreen';
+import SettingsScreen from './screens/SettingsScreen/SettingsScreen';
+
+// Export settings feature as default
+const Settings = {
+  SettingsScreen
+};
+
+export default Settings;
