c9e6706871a391120c026ca30c580300d452024e
Josef Ameur <148804322+ambrosia-fish@users.noreply.github.com>
2025-05-05 14:16:31 -0400

    fix(auth): add default export to auth/components/index.ts

--- app/features/auth/components/index.ts
+++ app/features/auth/components/index.ts
@@ -1,1 +1,9 @@
 export { default as ApprovalPendingModal } from './ApprovalPendingModal/ApprovalPendingModal';
+import ApprovalPendingModal from './ApprovalPendingModal/ApprovalPendingModal';
+
+// Export auth components as default
+const AuthComponents = {
+  ApprovalPendingModal
+};
+
+export default AuthComponents;
