25c9b8719f63d10676f1ce21ef281698f7f4e672
Josef Ameur <148804322+ambrosia-fish@users.noreply.github.com>
2025-05-05 14:16:46 -0400

    fix(auth): add default export to auth/index.ts

--- app/features/auth/index.ts
+++ app/features/auth/index.ts
@@ -1,3 +1,16 @@
 export * from './screens';
 export * from './components';
 export * from './utils';
+
+import * as Screens from './screens';
+import * as Components from './components';
+import * as Utils from './utils';
+
+// Export auth feature as default
+const Auth = {
+  Screens,
+  Components,
+  Utils
+};
+
+export default Auth;
