b43df3c2c16f0d035aeb91acde9ea9d6b6272494
Josef Ameur <148804322+ambrosia-fish@users.noreply.github.com>
2025-05-05 14:17:50 -0400

    fix(strategies): add default export to strategies/index.ts

--- app/features/strategies/index.ts
+++ app/features/strategies/index.ts
@@ -3,3 +3,20 @@ export { default as UseStrategyScreen } from './screens/UseStrategyScreen/UseStr
 export { default as StrategyCard } from './components/StrategyCard/StrategyCard';
 export { default as StrategyModal } from './components/StrategyModal/StrategyModal';
 export { default as TriggerPatterns } from './components/TriggerPatterns/TriggerPatterns';
+
+import NewStrategyScreen from './screens/NewStrategyScreen/NewStrategyScreen';
+import UseStrategyScreen from './screens/UseStrategyScreen/UseStrategyScreen';
+import StrategyCard from './components/StrategyCard/StrategyCard';
+import StrategyModal from './components/StrategyModal/StrategyModal';
+import TriggerPatterns from './components/TriggerPatterns/TriggerPatterns';
+
+// Export strategies feature as default
+const Strategies = {
+  NewStrategyScreen,
+  UseStrategyScreen,
+  StrategyCard,
+  StrategyModal,
+  TriggerPatterns
+};
+
+export default Strategies;
